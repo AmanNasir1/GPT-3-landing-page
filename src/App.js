1ee0f1fb1ee905afb4513886c8306e6c4cfc0b0a
Aman Nasir <aman.nasir231@gmail.com>
2023-02-04 20:48:45 +0500

    Added Navbar

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Gpt,
+  Header,
+} from "./containers/index";
+import { CTA, Brand, Navbar } from "./components/index";
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Gpt />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
